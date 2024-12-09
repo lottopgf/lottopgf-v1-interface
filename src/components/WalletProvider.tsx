5de40673e89166fd6d2b9899a771e708240a40ed
Kevin Charm <kevin@kevincharm.com>
2024-12-09 18:43:48 +0100

    initial commit

---
+++ src/components/WalletProvider.tsx
@@ -0,0 +1,39 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  lightTheme,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, base, scroll } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useColorMode } from "./ui/color-mode";
+
+const wagmiConfig = getDefaultConfig({
+  appName: "LottoPGF v1 Deployer",
+  projectId: "fuck_walletconnect",
+  chains: [mainnet, base, scroll],
+  // transports: [ // TODO
+  //   [mainnet.id]: http('url')
+  // ],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
+export function WalletProvider({ children }: { children: React.ReactNode }) {
+  const { colorMode } = useColorMode();
+
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={colorMode === "dark" ? darkTheme() : lightTheme()}
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
