da130cff1b1de6dd1b6a89ef3206cc52fcfbd8e3
Kevin Charm <kevin@kevincharm.com>
2024-12-16 16:32:27 +0000

    use hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './components/ThemeProvider.tsx'
 import { WalletProvider } from './components/WalletProvider.tsx'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createHashHistory } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen.ts'
 
-const router = createRouter({ routeTree })
+const history = createHashHistory()
+const router = createRouter({ routeTree, history })
 
 declare module '@tanstack/react-router' {
     interface Register {
