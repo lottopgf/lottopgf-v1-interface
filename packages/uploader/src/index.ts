c59830003a842492afacb0c11e7437d54cfa92c5
Kevin Charm <kevin@kevincharm.com>
2024-12-18 02:19:36 +0000

    use env port

--- packages/uploader/src/index.ts
+++ packages/uploader/src/index.ts
@@ -3,7 +3,9 @@ import { fastify } from './server'
 // Run the server!
 async function main() {
     try {
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({
+            port: Number(process.env.PORT) || 3000,
+        })
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
