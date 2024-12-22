3e6261859949d1c0b60574b3ca3f0065af2d2d92
Kevin Charm <kevin@kevincharm.com>
2024-12-22 16:27:54 +0100

    show info if on operator page while not being operator

--- packages/frontend/src/pages/Admin.tsx
+++ packages/frontend/src/pages/Admin.tsx
@@ -4,9 +4,15 @@ import { ErrorBoundary } from 'react-error-boundary'
 import { SeedJackpot } from '@/components/SeedJackpot'
 import { Kill } from '@/components/Kill'
 import { WithdrawAccruedFees } from '@/components/WithdrawAccruedFees'
+import { useLootery } from '@/hooks/useLootery'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { AlertTriangleIcon } from 'lucide-react'
 
 export function Admin({ chainId, contractAddress }: { chainId: number; contractAddress: Address }) {
     const currentChainId = useChainId()
+    const {
+        data: { isConnectedAccountOwner },
+    } = useLootery(contractAddress)
 
     if (currentChainId !== chainId) {
         return <div>Switch to the correct chain ({chainId}) to view tickets</div>
@@ -15,6 +21,22 @@ export function Admin({ chainId, contractAddress }: { chainId: number; contractA
     return (
         <div className="mb-4 space-y-14">
             <h1 className="text-4xl font-normal mt-16">Operator Functions</h1>
+
+            {!isConnectedAccountOwner && (
+                <Alert className="bg-red-500 text-white">
+                    <AlertTriangleIcon className="h-4 w-4 stroke-white" />
+                    <div />
+                    <div className="flex items-center justify-between gap-6">
+                        <div className="flex-1">
+                            <AlertTitle>You are not the operator of this lottery</AlertTitle>
+                            <AlertDescription className="space-y-2">
+                                You will not be able to perform operator-only functions.
+                            </AlertDescription>
+                        </div>
+                    </div>
+                </Alert>
+            )}
+
             <div className="mt-16 space-y-4">
                 <h2 className="text-2xl">Seed jackpot</h2>
                 <ErrorBoundary fallbackRender={({ error }) => <div>{error.message}</div>}>
